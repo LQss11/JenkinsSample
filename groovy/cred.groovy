0bc7dedec939362470a3db201f842606f800e27c
Salem Affes <affessalem@hotmail.fr>
2022-04-03 18:10:48 +0100

    Remove old groovy scripts

--- groovy/cred.groovy
+++
@@ -1,9 +0,0 @@
-////https://support.cloudbees.com/hc/en-us/articles/217708168-Creating-credentials-using-Groovy
-
-import com.cloudbees.plugins.credentials.impl.*
-import com.cloudbees.plugins.credentials.*
-import com.cloudbees.plugins.credentials.domains.*
-
-def env = System.getenv()
-Credentials cred = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,env.ID, "Description", env.USER, env.PASSWORD)
-SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), cred)
