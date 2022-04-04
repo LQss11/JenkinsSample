044cf7322f369bcefb43cd384b7dbc53b48a4b9d
Salem Affes <affessalem@hotmail.fr>
2022-04-04 08:25:27 +0100

    Update typo

--- variants/kubectl/load-kubeconfig.groovy
+++ variants/kubectl/load-kubeconfig.groovy
@@ -6,6 +6,6 @@ import java.nio.file.*;
 Path fileLocation = Paths.get("/kube/config");
 
 def secretBytes = SecretBytes.fromBytes(Files.readAllBytes(fileLocation))
-def credentials = new FileCredentialsImpl(CredentialsScope.GLOBAL, 'kube', 'Kubernetes config file for kubernetes credentials access', 'kubeconfigfil', secretBytes)
+def credentials = new FileCredentialsImpl(CredentialsScope.GLOBAL, 'kube', 'Kubernetes config file for kubernetes credentials access', 'kubeconfigfile', secretBytes)
 
 SystemCredentialsProvider.instance.store.addCredentials(Domain.global(), credentials)
