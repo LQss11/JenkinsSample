982a655ade0ad26c490ea85b816ef93ba798781a
Salem Affes <affessalem@hotmail.fr>
2022-05-05 14:26:05 +0100

    Update kubeconfig path

--- variants/kubectl/load-kubeconfig.groovy
+++ variants/kubectl/load-kubeconfig.groovy
@@ -3,7 +3,7 @@ import com.cloudbees.plugins.credentials.domains.Domain;
 import org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl;
 import java.nio.file.*;
 
-Path fileLocation = Paths.get("/kube/config");
+Path fileLocation = Paths.get("/root/.kube/config");
 
 def secretBytes = SecretBytes.fromBytes(Files.readAllBytes(fileLocation))
 def credentials = new FileCredentialsImpl(CredentialsScope.GLOBAL, 'kube', 'Kubernetes config file for kubernetes credentials access', 'kubeconfigfile', secretBytes)
